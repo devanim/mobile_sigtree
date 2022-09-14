c733193970e029c700a7c8b81696b7997bf9c543
Andrei Dana <andrei.dana87@gmail.com>
2022-09-14 21:14:34 +0300

    Add user screen component

--- src/screens/user/user-screen.tsx
+++ src/screens/user/user-screen.tsx
@@ -1,11 +1,14 @@
 import { userScreenStyle } from "./user-screen-style";
+import { Text } from "react-native";
 
 import Container from "src/components/Container";
 
-const UserScreen = () => {
+const UserScreen = (): JSX.Element => {
   return (<Container style={userScreenStyle.container}>
     <Text>
       {"Articles list placeholder here"}
     </Text>
   </Container>);
-}
+};
+
+export default UserScreen;
