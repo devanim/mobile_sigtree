a63f63d60fb83014c9e7a993844c78aeaf497a55
Andrei Dana <andrei.dana87@gmail.com>
2022-09-20 15:28:57 +0300

    add pagination for articles

--- src/screens/articles/article-list.styles.ts
+++ src/screens/articles/article-list.styles.ts
@@ -4,5 +4,11 @@ export const articleListStyles = StyleSheet.create({
   contentContainerStyle: {
     paddingHorizontal: 32,
     paddingBottom: 60,
-  }
+  }, 
+  footerText: {
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10
+  },
 });
--- src/screens/articles/articles-list.tsx
+++ src/screens/articles/articles-list.tsx
@@ -1,30 +1,54 @@
 import { useCallback, useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, View, ActivityIndicator } from "react-native";
 
-import { ScrollView } from "react-native-gesture-handler";
 import ArticleBrief from "../../models/article/article-brief";
 import { ResponseStatus } from "../../utils/response-status-enum";
 import ArticleBriefCard from "./article-brief-card";
 import { articleListStyles } from "./article-list.styles";
-import { mockArticles } from "./mock-articles";
+import { paginationMock } from "./mock-articles";
+import Text from "../../components/Text";
 
 const ArticlesList = (props: ArticleListProps): JSX.Element => {
   const [articles, setArticles] = useState<ArticleBrief[] | undefined>(undefined);
+  const [page, setPage] = useState(0);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [isLoadingData, setIsLoadingData] = useState(false);
 
   useEffect(() => {
+    setIsLoadingData(true);
     //TODO - replace with axios call to back-end
-    const requestResponse = mockArticles;
+    const requestResponse = paginationMock[page];
 
+    setIsLoadingData(false);
     if (requestResponse.status === ResponseStatus.fail) {
       alert("Could not retrieve data from back-end");
     }
 
-    setArticles(requestResponse.data?.articles);
-  }, []);
+    if (!articles) {
+      setArticles(requestResponse.data?.articles);
+      return;
+    }
+
+    setArticles([...articles, ...requestResponse.data?.articles ?? []]);
+    setHasNextPage(requestResponse.data?.more ?? false);
+  }, [page]);
+
+  const fetchNextPage = () => {
+    if (hasNextPage) {
+      setPage(page + 1);
+    }
+  }
 
   const onArticleSelected = (articleId: number) => {
     props.onArticleSelected(articleId);
   }
+
+  const renderFooter = () => (
+    <View style={articleListStyles.footerText}>
+        {isLoadingData && <ActivityIndicator />}    
+        {!hasNextPage && <Text>No more articles at the moment</Text>}
+    </View>
+)
   
   const renderItem = useCallback(({ item }) => {
     return <ArticleBriefCard key={item.id} articleBrief={item} onArticleSelected={onArticleSelected}/>
@@ -38,6 +62,9 @@ const ArticlesList = (props: ArticleListProps): JSX.Element => {
       showsVerticalScrollIndicator={false}
       scrollEventThrottle={16}
       contentContainerStyle={articleListStyles.contentContainerStyle}
+      onEndReachedThreshold={0.2}
+      onEndReached={fetchNextPage}
+      ListFooterComponent={renderFooter}
     />
   );
 };
