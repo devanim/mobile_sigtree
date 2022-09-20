090ed557175e214c44378ef947632dec1723de9a
Andrei Dana <andrei.dana87@gmail.com>
2022-09-20 21:55:25 +0300

    create style for article list

--- src/screens/articles/article-brief-card.tsx
+++ src/screens/articles/article-brief-card.tsx
@@ -2,6 +2,7 @@ import {  Pressable, View } from "react-native";
 import Text from "../../components/Text";
 
 import ArticleBrief from "src/models/article/article-brief";
+import { articleBriefStyles } from "./article-brief-styles";
 
 const ArticleBriefCard = (props: ArticleBriefCardProps): JSX.Element => {
   const onArticlePress = (articleId: number) => {
@@ -10,20 +11,14 @@ const ArticleBriefCard = (props: ArticleBriefCardProps): JSX.Element => {
 
   return (
     <Pressable onPress={() => onArticlePress(props.articleBrief.id)}>
-      <View>
-        <Text marginTop={48} category="title1">
+      <View style={articleBriefStyles.containerCard}>
+        <Text style={articleBriefStyles.default} category="title1">
           {props.articleBrief.title}
         </Text>
-        <Text marginTop={8} category="call-out" status="placeholder">
-          {props.articleBrief.id}
-        </Text>
-        <Text marginTop={8} category="call-out" status="placeholder">
+        <Text style={articleBriefStyles.default} category="call-out" status="placeholder">
           {props.articleBrief.excerpt}
         </Text>
-        <Text marginTop={8} category="call-out" status="placeholder">
-          {props.articleBrief.tags}
-        </Text>
-        <Text marginTop={8} category="call-out" status="placeholder">
+        <Text style={articleBriefStyles.default} category="call-out" status="placeholder">
           {props.articleBrief.timestamp}
         </Text>
       </View>
---
+++ src/screens/articles/article-brief-styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const articleBriefStyles = StyleSheet.create({
+  containerCard: {
+    backgroundColor: "#fff",
+    marginTop: 10,
+    borderRadius: 15
+  },
+  default: {
+    margin: 8,
+    color: "#000",
+  }
+});
