d80a1587d5a4915fd79126e9855b2e5a627b76b3
Andrei Dana <andrei.dana87@gmail.com>
2022-09-26 21:48:36 +0300

    add article carousel

--- src/screens/dashboard/dashboard-screen.tsx
+++ src/screens/dashboard/dashboard-screen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "@ui-kitten/components";
 
 import Container from "../../components/container";
@@ -9,10 +9,38 @@ import { ScrollView } from "react-native";
 import { RefreshControl } from "react-native-web-refresh-control";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { AppStackParamList } from "../../routing/route-screens";
+import Carousel from "../../components/carousel/carousel";
+import ArticleBrief from "../../models/article/article-brief";
+import { ResponseStatus } from "../../utils/response-status-enum";
+import { mockArticles } from "../articles/mock-articles";
 
 const DashboardScreen = (): JSX.Element => {
   const layoutData = useLayout();
   const { navigate } = useNavigation<NavigationProp<AppStackParamList>>();
+  const [articles, setArticles] = useState<ArticleBrief[] | undefined>([]);
+
+  useEffect(() => {
+    //TODO - replace with axios call to back-end
+    const requestResponse = mockArticles;
+
+    if (requestResponse.status === ResponseStatus.fail) {
+      alert("Could not retrieve articles data from back-end");
+    }
+
+    setArticles(requestResponse.data?.articles);
+  }, []);
+
+  const onArticleSelected = (article: ArticleBrief) => {
+    //TODO - redirect to article screen
+    alert(`Selected article with title ${article.title}. Soon it will redirect there`);
+  }
+
+  const articleCarouselData = articles?.map((article: ArticleBrief) => {
+    return {
+      data: article,
+      onSelected: () => onArticleSelected(article)
+    }
+  });
 
   return (<Container style={dashboardStyles.container}>
     <ScrollView
@@ -20,6 +48,7 @@ const DashboardScreen = (): JSX.Element => {
         showsVerticalScrollIndicator={false}
         refreshControl={<RefreshControl tintColor="#F0DF67" />}
       >
+        <Carousel name={"Articles"} data={articleCarouselData ?? []}/>
         {/* <Header />
         <BestSeller />
         <Gallery /> */}
