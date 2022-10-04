f7aaa676540ccd0a4fb93b84566a4a125cfb0082
Andrei Dana <andrei.dana87@gmail.com>
2022-10-04 21:22:12 +0300

    Add custom checkbox component

---
+++ src/components/cust-checkbox-styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const custCheckbokStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 32,
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
+  },
+});
+
+export default custCheckbokStyles;
