718a290eecf40568c51e18c2514bcd3f6102798d
Andrei Dana <andrei.dana87@gmail.com>
2022-10-04 19:31:56 +0300

    small fixes

--- src/components/ad-mob.tsx
+++ src/components/ad-mob.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
 import { AdMobBanner } from "expo-ads-admob";
 
 type AdMobProps = {
