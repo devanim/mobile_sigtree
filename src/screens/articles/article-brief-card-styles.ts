c98446fd8a5bc38285076c4a068bf6bdd728db4b
Andrei Dana <andrei.dana87@gmail.com>
2022-10-05 20:20:38 +0300

    Improve article style

---
+++ src/screens/articles/article-brief-card-styles.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export const articleBriefCardStyles = StyleSheet.create({
+  containerCard: {
+    backgroundColor: "#fff",
+    width: "100%",
+    marginBottom: 1,
+    borderBottomColor: "#000",
+    flex: 2,
+    flexDirection: "row"
+  },
+  text: {
+    margin: 5,
+    color: "#000",
+  },
+  dateText: {
+    margin: 5,
+    color: "#000",
+    fontSize: 10,
+  }, 
+  title: {
+    margin: 5,
+    color: "#000",
+    fontWeight: "bold"
+  }, 
+  textView: {
+    flex: 3,
+    padding: 5,
+    flexDirection: "column"
+  },
+  image: {
+    flex: 1,
+    width: '25%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  dateView: {
+    width: '17%',
+    alignContent: "center",
+    marginTop: '5%'
+  }
+});
--- src/screens/articles/article-brief-card.tsx
+++ src/screens/articles/article-brief-card.tsx
@@ -1,29 +1,26 @@
-import {  Pressable, View, Image } from "react-native";
-import Text from "../../components/text";
+import { Pressable, View, Image, Text } from "react-native";
 
 import ArticleBrief from "src/models/article/article-brief";
-import { articleBriefStyles } from "./article-brief-styles";
+import { articleBriefCardStyles } from "./article-brief-card-styles";
 
 const ArticleBriefCard = (props: ArticleBriefCardProps): JSX.Element => {
+  const localizedDate = new Date(props.articleBrief.timestamp).toLocaleDateString();
+
   const onArticlePress = (articleId: number) => {
     props.onArticleSelected(articleId);
   }
 
   return (
     <Pressable onPress={() => onArticlePress(props.articleBrief.id)}>
-      <View style={articleBriefStyles.containerCard}>
-        <View style={articleBriefStyles.firstRow}>
-          <Text style={articleBriefStyles.title} category="title1">
-            {props.articleBrief.title}
-          </Text>
-          <Image style={articleBriefStyles.image} source={{uri: `${props.articleBrief.image}`}}/>
+      <View style={articleBriefCardStyles.containerCard}>
+        <Image style={articleBriefCardStyles.image} source={{uri: `${props.articleBrief.image}`}}/>
+        <View style={articleBriefCardStyles.textView}>
+          <Text style={articleBriefCardStyles.title}>{props.articleBrief.title}</Text>
+          <Text style={articleBriefCardStyles.text}>{props.articleBrief.excerpt}</Text>
         </View>
-        <Text style={articleBriefStyles.default} category="call-out" status="placeholder">
-          {props.articleBrief.excerpt}
-        </Text>
-        <Text style={articleBriefStyles.default} category="call-out" status="placeholder">
-          {props.articleBrief.timestamp}
-        </Text>
+        <View style={articleBriefCardStyles.dateView}>
+          <Text style={articleBriefCardStyles.dateText}>{localizedDate}</Text>
+        </View>
       </View>
     </Pressable>
   );
