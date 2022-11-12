a8739c211c38a9fc1a0598342f2dca9de10a386b
Andrei Dana <andrei.dana87@gmail.com>
2022-11-12 22:03:14 +0200

    add translations key

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const REALMS_KEY = "subscribed-realms";
 export const APP_LANGUAGE_KEY = "selected-app-language";
-export const APP_LANGUAGE_DETAILS_KEY = "saved-language-details";
+export const APP_TRANSLATIONS_KEY = "saved-app-translations";
